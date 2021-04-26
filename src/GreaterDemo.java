871426461f70a5e0815f3351515c2448ef027c57
My_programs <ravinagade1997@gmail.com>
2021-04-26 10:55:52 +0530

    Initial commit

---
+++ src/GreaterDemo.java
@@ -0,0 +1,15 @@
+public class GreaterDemo {
+    public static void main(String[] args){
+        int a=1;
+        int b=5;
+        int c=20;
+
+        if(a>b){
+            System.out.println("a is the greater number");
+        }
+        else{
+            System.out.println("c is greater");
+        }
+
+    }
+}
---
+++ src/NaturalDemo.java
@@ -0,0 +1,11 @@
+public class NaturalDemo {
+
+    public static void main(String[] args) {
+
+        int i, num = 10, sum = 0;
+        for (i = 1; i <= num; ++i) {
+            sum = sum + i;
+        }
+        System.out.println("sum of first 10 natural number is=" + sum);
+    }
+}
